f7988b51084634501016c9b543d130013ffb87b3
golubevc <golubevalexeyd@gmail.com>
2025-06-01 11:25:56 +0300

    init

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"coffee-service/internal/handler"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	db, err := database.initDB()
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	coffeeHandler := handler.NewCoffeeHandler(db)
+
+	http.HandleFunc("/coffees", coffeeHandler.GetAllCoffees)
+
+	fmt.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
